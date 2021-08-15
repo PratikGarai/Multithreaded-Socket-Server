ff3850ab3f389006b2f6c91d334db63ed44750cd
PratikGarai <pratikgarai0208@gmail.com>
2021-08-16 03:37:36 +0530

    server completed

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 
 import socket 
 from colorama import Fore
+import pickle
 
 
 # Globals
@@ -10,7 +11,9 @@ SOCKET_TYPE = socket.SOCK_STREAM
 SOCKET_HOST = "localhost"
 SOCKET_PORT = 3500
 SOCKET_INCOMING_LIMIT = 1
+SOCKET_ACCEPT_TIMEOUT = 5
 
+BUFFER_SIZE = 8         # Bytes
 
 # Server starts here
 
@@ -28,5 +31,29 @@ print(Fore.CYAN + f"Socket is bound to {SOCKET_HOST} on port {SOCKET_PORT}.")
 soc.listen(SOCKET_INCOMING_LIMIT)
 print(Fore.CYAN + "Listening for incoming connection ...")
 
-connection, address = soc.accept()
-print(Fore.GREEN + f"Connected to a client: {address}.")
+soc.settimeout(SOCKET_ACCEPT_TIMEOUT)
+connected = False
+try:
+    connection, address = soc.accept()
+    print(Fore.GREEN + f"Connected to a client: {address}.")
+    connected = True
+except socket.timeout:
+    print(Fore.RED + "The server socket has timed out.")
+    soc.close()
+
+if connected:
+    received_data = connection.recv(BUFFER_SIZE)
+    received_data = pickle.loads(received_data)
+    print(Fore.GREEN+f"Received data from the client: {received_data}")
+    
+    msg = "Reply from the server."
+    msg = pickle.loads(msg)
+    connection.sendall(msg)
+    print(Fore.GREEN+"Server sent a message to the client.")
+
+    connection.close()
+    print(Fore.CYAN+f"Connection is closed with: {address}.")
+
+soc.close()
+print(Fore.GREEN+"\nSocket is closed.")
+print(Fore.WHITE)
